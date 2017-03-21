70c883bf974c5bdd37cad3d1b7ba565b2b6d2c79
Aaron Nguyen and Max Reichman <max@>
2017-03-21 11:23:23 -0700

    Set up the project

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#p1").click(function() {
+    $( this ).toggleClass("highlighted");
+  });
+  $("#p2").click(function() {
+    $( this ).addClass("highlighted");
+  });
+  $("#p3").click(function() {
+    $( this ).addClass("highlighted");
+  });
+});
