1c9fc6b8de9e712c0f03f3e693f0f3efa6d92aa7
Aaron Nguyen and Max Reichman <max@>
2017-03-21 12:02:05 -0700

    Add JavaScript to toggle definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
-  $("#p1").click(function() {
-    $( this ).toggleClass("highlighted");
-  });
-  $("#p2").click(function() {
-    $( this ).addClass("highlighted");
-  });
-  $("#p3").click(function() {
-    $( this ).addClass("highlighted");
+  $(".panel-heading").click(function() {
+    $(this).next().toggle();
   });
 });
